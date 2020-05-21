aab1938b39d20b3e5a8f66f908f95050b6a798fc
KarimGil <karimg87@hotmail.com>
2020-05-20 23:39:33 -0700

    deployy

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+%qm7(^%pld$ndhv_n9g4ck&bp&1$qenna%_k06p297+syj+_2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://trendscrapper.herokuapp.com/']
 
